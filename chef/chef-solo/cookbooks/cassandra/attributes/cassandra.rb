3fbb998a1853afe91e7f553203840c774613b0f9
Sal PimpSkin(tm) Fuentes <fuentesjr@gmail.com>
2010-08-16 01:11:01 -0700

    new attribute DSL

--- chef/chef-solo/cookbooks/cassandra/attributes/cassandra.rb
+++ chef/chef-solo/cookbooks/cassandra/attributes/cassandra.rb
@@ -1,4 +1,4 @@
-# STRUCTURE OF THE CASSANDRA DATA BAG (meaning a databag named 'cassandra')
+# STRUCTURE OF THE CASSANDRA DATA
 # 
 #   {:id : "clusters",
 #     {<cluster name> =>
@@ -47,36 +47,36 @@ example_keyspace_config = {
   }
 }
 
-set_unless[:cassandra][:cluster_name]  = "TestCluster"
-set_unless[:cassandra][:auto_bootstrap]  = false
-set_unless[:cassandra][:keyspaces] = example_keyspace_config
-set_unless[:cassandra][:authenticator] = "org.apache.cassandra.auth.AllowAllAuthenticator"
-set_unless[:cassandra][:partitioner] = "org.apache.cassandra.dht.OrderPreservingPartitioner"
-set_unless[:cassandra][:initial_token] = ""
-set_unless[:cassandra][:commit_log_dir] = "/var/lib/cassandra/commitlog"
-set_unless[:cassandra][:data_file_dirs] = ["/var/lib/cassandra/data"]
-set_unless[:cassandra][:callout_location] = "/var/lib/cassandra/callouts"
-set_unless[:cassandra][:staging_file_dir] = "/var/lib/cassandra/staging"
-set_unless[:cassandra][:seeds] = ["127.0.0.1"]
-set_unless[:cassandra][:rpc_timeout] = 5000
-set_unless[:cassandra][:commit_log_rotation_threshold] = 128
-set_unless[:cassandra][:jmx_port] = 8080
-set_unless[:cassandra][:listen_addr] = "localhost"
-set_unless[:cassandra][:storage_port] = 7000
-set_unless[:cassandra][:thrift_addr] = "localhost"
-set_unless[:cassandra][:thrift_port] = 9160
-set_unless[:cassandra][:thrift_framed_transport] = false
-set_unless[:cassandra][:disk_access_mode] = "auto"
-set_unless[:cassandra][:sliced_buffer_size] = 64
-set_unless[:cassandra][:flush_data_buffer_size] = 32
-set_unless[:cassandra][:flush_index_buffer_size] = 8
-set_unless[:cassandra][:column_index_size] = 64
-set_unless[:cassandra][:memtable_throughput] = 64
-set_unless[:cassandra][:binary_memtable_throughput] = 256
-set_unless[:cassandra][:memtable_ops] = 0.3
-set_unless[:cassandra][:memtable_flush_after] = 60
-set_unless[:cassandra][:concurrent_reads] = 8
-set_unless[:cassandra][:concurrent_writes] = 32
-set_unless[:cassandra][:commit_log_sync] = "periodic"
-set_unless[:cassandra][:commit_log_sync_period] = 10000
-set_unless[:cassandra][:gc_grace] = 864000
+default.cassandra.cluster_name  = "TestCluster"
+default.cassandra.auto_bootstrap  = false
+default.cassandra.keyspaces = example_keyspace_config
+default.cassandra.authenticator = "org.apache.cassandra.auth.AllowAllAuthenticator"
+default.cassandra.partitioner = "org.apache.cassandra.dht.OrderPreservingPartitioner"
+default.cassandra.initial_token = ""
+default.cassandra.commit_log_dir = "/var/lib/cassandra/commitlog"
+default.cassandra.data_file_dirs = ["/var/lib/cassandra/data"]
+default.cassandra.callout_location = "/var/lib/cassandra/callouts"
+default.cassandra.staging_file_dir = "/var/lib/cassandra/staging"
+default.cassandra.seeds = ["127.0.0.1"]
+default.cassandra.rpc_timeout = 5000
+default.cassandra.commit_log_rotation_threshold = 128
+default.cassandra.jmx_port = 8080
+default.cassandra.listen_addr = "localhost"
+default.cassandra.storage_port = 7000
+default.cassandra.thrift_addr = "localhost"
+default.cassandra.thrift_port = 9160
+default.cassandra.thrift_framed_transport = false
+default.cassandra.disk_access_mode = "auto"
+default.cassandra.sliced_buffer_size = 64
+default.cassandra.flush_data_buffer_size = 32
+default.cassandra.flush_index_buffer_size = 8
+default.cassandra.column_index_size = 64
+default.cassandra.memtable_throughput = 64
+default.cassandra.binary_memtable_throughput = 256
+default.cassandra.memtable_ops = 0.3
+default.cassandra.memtable_flush_after = 60
+default.cassandra.concurrent_reads = 8
+default.cassandra.concurrent_writes = 32
+default.cassandra.commit_log_sync = "periodic"
+default.cassandra.commit_log_sync_period = 10000
+default.cassandra.gc_grace = 864000
