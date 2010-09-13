a1435f83268426808433f6336a68fa505232970b
Sal PimpSkin(tm) Fuentes <fuentesjr@gmail.com>
2010-09-13 00:14:59 -0700

    fix mod_auth_openid on rhs and rhs-like systems

--- chef/chef-solo/cookbooks/apache2/recipes/mod_auth_openid.rb
+++ chef/chef-solo/cookbooks/apache2/recipes/mod_auth_openid.rb
@@ -19,46 +19,80 @@
 
 case node[:platform]
 when "centos", "redhat", "fedora"
+  %w{gcc-c++ openssl openssl-devel curl-devel expat-devel libtidy-devel 
+    httpd-devel sqlite-devel pcre-devel mysql-devel}.each do |pkg|
+    package pkg
+  end
 
+  remote_file "/tmp/libopkele-2.0.4.tar.gz" do
+    source "http://kin.klever.net/dist/libopkele-2.0.4.tar.gz"
+    mode 0644
+  end
+
+  bash "install libopkele" do
+    cwd "/tmp"
+    code <<-EOH
+    tar zxvf libopkele-2.0.4.tar.gz
+    cd libopkele-2.0.4 && ./configure --prefix=/usr
+    make && make install
+    cp /usr/lib64/libexpat.so /usr/lib/libexpat.so 
+    EOH
+    # temporary solution: http://keystoneit.wordpress.com/2008/03/05/storegrid-installation-on-64-bit-rhel-centos-systems/
+    not_if { File.exists?("/usr/lib/pkgconfig/libopkele.pc") }
+  end
 when "debian", "ubuntu"
   %w{ apache2-prefork-dev libopkele-dev libopkele3 }.each do |pkg|
     package pkg
   end
+end
 
-  remote_file "/tmp/mod_auth_openid-0.4.tar.gz" do
-    source "http://butterfat.net/releases/mod_auth_openid/mod_auth_openid-0.4.tar.gz"
-    mode 0644
-  end
+remote_file "/tmp/mod_auth_openid-0.4.tar.gz" do
+  source "http://butterfat.net/releases/mod_auth_openid/mod_auth_openid-0.4.tar.gz"
+  mode 0644
+end
 
-  bash "install mod_auth_openid" do
-    cwd "/tmp"
-    code <<-EOH
-    tar zxvf mod_auth_openid-0.4.tar.gz
-    cd mod_auth_openid-0.4 && ./configure
-    perl -pi -e "s/-i -a -n 'authopenid'/-i -n 'authopenid'/g" Makefile
-    make && make install
-    EOH
-    not_if { File.exists?("/usr/lib/apache2/modules/mod_auth_openid.so") }
-  end
+bash "install mod_auth_openid" do
+  cwd "/tmp"
+  code <<-EOH
+  export PKG_CONFIG_PATH=/usr/lib/pkgconfig:$PKG_CONFIG_PATH
+  tar zxvf mod_auth_openid-0.4.tar.gz
+  cd mod_auth_openid-0.4 && ./configure
+  perl -pi -e "s/-i -a -n 'authopenid'/-i -n 'authopenid'/g" Makefile
+  make && make install
+  EOH
+  not_if { File.exists?("/usr/lib/apache2/modules/mod_auth_openid.so") }
+end
 
-  file "/var/cache/apache2/mod_auth_openid.db" do
-    owner node[:apache][:user]
-    mode 0600
-  end
+directory "/var/cache/apache2" do
+  owner node[:apache][:user]
+  mode 0755
+  recursive true
+end
 
-  template "#{node[:apache][:dir]}/mods-available/authopenid.load" do
-    source "mods/authopenid.load.erb"
-    owner "root"
-    group "root"
-    mode 0644
-  end
+file "/var/cache/apache2/mod_auth_openid.db" do
+  owner node[:apache][:user]
+  mode 0600
+end
 
-  apache_module "authopenid"
-
-  template "/usr/local/bin/mod_auth_openid.rb" do
-    source "mod_auth_openid.rb.erb"
-    owner node[:apache][:user]
-    group node[:apache][:user]
-    mode 0750
+template "#{node[:apache][:dir]}/mods-available/authopenid.load" do
+  source "mods/authopenid.load.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  case node[:platform]
+  when "centos", "redhat", "fedora"
+    variables :libdir => "lib64", :apache_name => "httpd"
+  when "debian", "ubuntu"
+    variables :libdir => "lib", :apache_name => "apache2"
   end
 end
+
+apache_module "authopenid"
+
+template "/usr/local/bin/mod_auth_openid.rb" do
+  source "mod_auth_openid.rb.erb"
+  owner node[:apache][:user]
+  group node[:apache][:user]
+  mode 0750
+end
+
